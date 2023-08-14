d832769d6fc725d76e5badddd94037efd5b308b5
Shakhmurod <66996045+skywoker007@users.noreply.github.com>
2023-08-14 19:45:16 +0000

    full style added

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 const elsTabsItem = document.querySelectorAll('.tabs__item');
-const elsTabsPanel = document.querySelectorAll('.tabs__panel');
+const elsTabsPanel = document.querySelectorAll('.tabpanels__item');
 const elsTabLink = document.querySelectorAll('.js-tab-link');
+
+const elsAccordionItem = document.querySelectorAll('.accordion__item');
+const elsAccordionItemToggler = document.querySelectorAll('.accordion__item-toggler');
+
 function deactivateTabItems(){
     elsTabsItem.forEach(function(elTabsItem){
         elTabsItem.classList.remove('tabs__item--active');
@@ -8,7 +12,13 @@ function deactivateTabItems(){
 }
 function deactivateTabPanels(){
     elsTabsPanel.forEach(function(elTabsPanel){
-        elTabsPanel.classList.remove('tabs__panel--active');
+        elTabsPanel.classList.remove('tabpanels__item--active');
+    });
+}
+
+function closeAccordionItems () {
+    elsAccordionItem.forEach(function (elAccordionItem) {
+        elAccordionItem.classList.remove('accordion__item--open');
     });
 }
 elsTabLink.forEach(function(elTabLink){
@@ -28,7 +38,15 @@ elsTabLink.forEach(function(elTabLink){
         // aktiv panelni ko`rsatish
         const elTargetPanel = document.querySelector(elTabLink.dataset.tabTarget);
 
-        elTargetPanel.classList.add('tabs__panel--active');
+        elTargetPanel.classList.add('tabpanels__item--active');
         
+    });
+});
+
+elsAccordionItemToggler.forEach(function (elAccordionItemToggler) {
+    elAccordionItemToggler.addEventListener('click', function () {
+        closeAccordionItems();
+
+        elAccordionItemToggler.closest('.accordion__item').classList.add('accordion__item--open');
     });
 });
